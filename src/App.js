36049983996229ea63d0156be8d9f21ba324c14e
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-11-06 13:06:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import Header from './components/Header/Header01/header';
+import { Routes, Route } from "react-router-dom";
+// import Header from './components/Header/Header01/Header';
+// import Profile from './PortfolioContainer/Home/Profile';
+// import Contact from './PortfolioContainer/ContactMe/Contact';
+// import AboutUs from './PortfolioContainer/AboutUs/AboutUs.js';
+// import Service from './PortfolioContainer/Service/index';
+// import Blog from './PortfolioContainer/Blog/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <>
+      <h1>Hello</h1>
+      {/* <Header /> */}
+      {/* <Routes>
+        <Route path="/" exact element={<Profile />} />
+        <Route path="contact" exact element={<Contact />} />
+        <Route path="about" exact element={<AboutUs />} />
+        <Route path="service" exact element={<Service />} />
+        <Route path="blog" exact element={<Blog />} />
+      </Routes> */}
+    </>
   );
 }
 
