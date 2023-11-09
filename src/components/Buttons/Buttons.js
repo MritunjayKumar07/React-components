4aa9b730e13f35f5619916bd922088f5543b4116
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-11-09 10:52:48 +0000

    commit

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,9 +1,167 @@
 import React from 'react'
+import ButtonsStyle from './Buttons.module.css'
 
 export default function Buttons() {
   return (
     <>
-      Buttons
+        <h3>Simple Buttons</h3>
+      <ul className={ButtonsStyle.ul}>
+        <li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 1">
+            <button className={ButtonsStyle.button_1} role="button">Button 1</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 2">
+            <button className={ButtonsStyle.button_2} role="button">Button 2</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 3">
+            <button className={ButtonsStyle.button_3} role="button">Button 3</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 4">
+            <button className={ButtonsStyle.button_4} role="button">Button 4</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 5">
+            <button className={ButtonsStyle.button_5} role="button">Button 5</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 6">
+            <button className={ButtonsStyle.button_6} role="button">Button 6</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 7">
+            <button className={ButtonsStyle.button_7} role="button">Button 7</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 8">
+            <button className={ButtonsStyle.button_8} role="button">Button 8</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 9">
+            <button className={ButtonsStyle.button_9} role="button">Button 9</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 10">
+            <button className={ButtonsStyle.button_10} role="button">Button 10</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 11">
+            <button className={ButtonsStyle.button_11} role="button">Button 11</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 12">
+            <button className={ButtonsStyle.button_12} role="button">Button 12</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 13">
+            <button className={ButtonsStyle.button_13} role="button">Button 13</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 14">
+            <button className={ButtonsStyle.button_14} role="button">Button 14</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 15">
+            <button className={ButtonsStyle.button_15} role="button">Button 15</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 16">
+            <button className={ButtonsStyle.button_16} role="button">Button 16</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 17">
+            <button className={ButtonsStyle.button_17} role="button">Button 17</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 18">
+            <button className={ButtonsStyle.button_18} role="button">Button 18</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 19">
+            <button className={ButtonsStyle.button_19} role="button">Button 19</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 20">
+            <button className={ButtonsStyle.button_20} role="button">Button 20</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 21">
+            <button className={ButtonsStyle.button_21} role="button">Button 21</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 22">
+            <button className={ButtonsStyle.button_22} role="button">Button 22</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 23">
+            <button className={ButtonsStyle.button_23} role="button">Button 23</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 24">
+            <button className={ButtonsStyle.button_24} role="button">Button 24</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 25">
+            <button className={ButtonsStyle.button_25} role="button">Button 25</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 26">
+            <button className={ButtonsStyle.button_26} role="button">Button 26</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 27">
+            <button className={ButtonsStyle.button_27} role="button">Button 27</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 28">
+            <button className={ButtonsStyle.button_28} role="button">Button 28</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 29">
+            <button className={ButtonsStyle.button_29} role="button">Button 29</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 30">
+            <button className={ButtonsStyle.button_30} role="button">Button 30</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 31">
+            <button className={ButtonsStyle.button_31} role="button">Button 31</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 32">
+            <button className={ButtonsStyle.button_32} role="button">Button 32</button>
+          </div>
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 33">
+            <button className={ButtonsStyle.button_33} role="button">Button 33</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 34">
+            <button className={ButtonsStyle.button_34} role="button">Button 34</button>
+          </div>
+
+        </li><li>
+          <div className={ButtonsStyle.button_wrapper} data-tippy-content="Click to copy button 35">
+            <button className={ButtonsStyle.button_35} role="button">Button 35</button>
+          </div>
+        </li>
+      </ul>
     </>
   )
 }
